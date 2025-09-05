f3a75992e4ab8b8dc5b52fcdc6f41fa2b494e9ef
Dae Dowdell <dow_dell2010@yahoo.com>
2025-09-05 12:23:50 -0400

    clue 2 and 3

--- clues.sql
+++ clues.sql
@@ -8,11 +8,15 @@ world=# select code, name, population from countries where region = 'Southern Eu
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
+world=# select language from countrylanguages where countrycode = 'VAT';
+
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
+world=# select * from countrylanguages where language = 'Italian' and percentage = 100;
+
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
