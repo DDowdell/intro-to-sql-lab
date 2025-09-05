877b1ed13d0ee974cd5db0c570b117efafb04d2d
Dae Dowdell <dow_dell2010@yahoo.com>
2025-09-05 12:37:35 -0400

    clue 5

--- clues.sql
+++ clues.sql
@@ -29,6 +29,8 @@ world=# select * from cities where countrycode = 'SMR' and name != 'San Marino';
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+world=# select name, countrycode from cities where name like 'Serra%' and countrycode != 'SMR';
+
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
